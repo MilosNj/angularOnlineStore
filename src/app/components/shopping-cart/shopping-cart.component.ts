3e5425001b48ee4cd4c2c81290af2399315e40c4
Milos Njegovanovic <njegovanovicm@yahoo.com>
2021-07-06 14:40:40 +0200

    Finished implementing shopping cart

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,7 +8,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
+  @ViewChild('price') price!: string;
+
   gameNames!: Array<string>;
+  displayedColumns: string[] = ['name', 'price'];
 
   constructor() { }
 
@@ -38,6 +42,27 @@ export class ShoppingCartComponent implements OnInit {
   emptyCart(): void {
     this.gameNames = [];
     localStorage.clear();
+    location.reload();
     console.log(this.gameNames);
+  }
+
+  buyCart(): void {
+    (async () => {
+      const { value: email } = await Swal.fire({
+        title: 'Input email address to receive game codes',
+        input: 'email',
+        inputLabel: 'Your email address',
+        inputPlaceholder: 'Enter your email address'
+      })
+
+      if (email) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Success!',
+          text: 'Thanks for buying games from us!'
+        })
+        setTimeout(this.emptyCart, 2000);
+      }
+    })()
   }
 }
--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -14,7 +14,7 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
                 'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com'
             },
             setParams: {
-                key: 'e40e743af2c94b0c916a8aa618fb4473'
+                key: 'a97a457624bf430a96e4801dbdc34717'
             }
         });
         return next.handle(req);
